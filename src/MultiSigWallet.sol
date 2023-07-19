2e0cf3ee645c9d730f9a206b541e968652953dec
Luke Sheppard <sheppiegit@gmail.com>
2023-07-19 14:10:36 -0400

    added most storage vairbales and constructor.

---
+++ src/MultiSigWallet.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract MultiSigWallet {
+    //events
+
+    //storage variables
+
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint256 public immutable numConfirmationsRequired;
+
+    struct Transaction {
+        address to;
+        uint256 value;
+        bytes32 data;
+        bool executed;
+        uint256 numConfirmations;
+    }
+
+    //mapping from tx index => owner => bool
+
+    Transaction[] public transactions;
+
+    //modifers
+
+    //constructor
+    constructor(address[] memory _owners, uint256 _numConfirmationsRequired) {
+        require(_owners.length > 0, "owners required");
+        require(
+            _numConfirmationsRequired > 0 && _numConfirmationsRequired <= _owners.length,
+            "invliad number of required confirmations"
+        );
+
+        for (uint256 i = 0; i < owners.length; i++) {
+            address owner = _owners[i];
+
+            require(owner != address(0), "invalid address");
+            require(!isOwner[owner], "owner must be unique");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+        numConfirmationsRequired = _numConfirmationsRequired;
+    }
+
+    //public functions
+
+    //public view functions
+}
